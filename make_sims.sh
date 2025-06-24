36af79a594b9c1573a16ade5b9330d273a2f89ca
rygolden17 <rygoldenm@gmail.com>
2025-06-24 00:03:02 -0700

    Update make_sims.sh

--- make_sims.sh
+++ make_sims.sh
@@ -15,11 +15,7 @@ for num in 3 4 5 6 7; do
     cat currents.h |sed s/'#define time2(a) (2)'/'#define time2(a) (RANDNUM)'/ >currents_temp.h
     cat currents_temp.h |sed s/RANDNUM/${num}/ >currents.h 
 
-    cat constructTrainedWeightFile.py |sed s/"num=2"/"num=RANDNUM"/ >constructTrainedWeightFile_tmp.py
-    cat constructTrainedWeightFile_tmp.py |sed s/RANDNUM/${num}/ >constructTrainedWeightFile.py
-    rm constructTrainedWeightFile_tmp.py
-
-    make
+    make mindcurrent
 done
 
 echo "The script ends now."
